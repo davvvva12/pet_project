8f7b72afe49b637546fefbcb8095c9dca6c81936
davvvva12 <davidviktaiut@gmail.com>
2026-05-03 20:09:28 +0200

    Добавьте файлы проекта.

---
+++ pet_project/pet_project.cpp
@@ -0,0 +1,19 @@
+﻿// pet_project.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+class Task_Manager {
+public:
+    Task_Manager() {
+
+    }
+
+
+private:
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
