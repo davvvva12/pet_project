5da654429f0b6e1389f9b17afafa7b354554d2ab
davvvva12 <davidviktaiut@gmail.com>
2026-05-09 22:20:31 +0200

    second over commit

--- pet_project/pet_project.cpp
+++ pet_project/pet_project.cpp
@@ -4,22 +4,44 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <string>
+#include <tuple>
+#include <future>
+#include <utility>
+#include <typeinfo>
 using namespace std;
-template<typename F>
+void print(int a) {
+    cout << a;
+}
 class Task_Manager {
 public:
     Task_Manager() {
 
     }
-    void add_work(functional<F> a) {
 
+    template< typename T, class ...F>
+    void add_work(T(*func)(F...),F ... arg) {
+
+        task_stack.push_back([=]() {
+            func(arg...);
+            });
     }
+    void run() {
+        for (auto x:task_stack) {
+            x();
+           }
+       
+    }
+private: 
+    
+    vector<function<void()>> task_stack;
 
-private:
-    vector<functional<F>> task_stack;
 };
 int main()
 {
-    std::cout << "Hello World!\n";
+    int d = 0;
+    Task_Manager obj;
+    obj.add_work(print, d);
+    obj.run();
 }
 
